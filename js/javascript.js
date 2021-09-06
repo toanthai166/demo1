080443f0bdb2541daea19041e6aec8222f66542b
toanthai166 <vutoanthai@gmail.com>
2021-09-06 21:57:17 +0700

    upload source code

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+
+// (function(){
+//     $('.carousel-showmanymoveone .item').each(function(){
+//       var itemToClone = $(this);
+  
+//       for (var i=1;i<6;i++) {
+//         itemToClone = itemToClone.next();
+  
+        
+//         if (!itemToClone.length) {
+//           itemToClone = $(this).siblings(':first');
+//         }
+  
+//         itemToClone.children(':first-child').clone()
+//           .addClass("cloneditem-"+(i))
+//           .appendTo($(this));
+//       }
+//     });
+//   }());
